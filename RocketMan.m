ea99ad601b3a004a68e6e43ccf44e695fada0753
szonglong <E0004621@u.nus.edu>
2018-03-21 14:04:37 +0800

    1.1
    Added in dry mass and gravitational potential

--- RocketMan.m
+++ RocketMan.m
@@ -8,20 +8,22 @@
 
 
 %static variables
-ve=10;
+ve=100;
 rho=1;
-dm=1;
+dm=1; md=60;
+G=10; RE=50; ME=10;
+
 
 %initialise values at t=0
 z0=0;
 v0=0;
-m0=540;
+m0=960;
 tau = 0.01; %interval
-maxstep = 1000; % no. of iterations
+maxstep = 2000; % no. of iterations
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %assigning variables to init values
-z=z0; v=v0; m=m0;
+z=z0; v=v0; mf=m0; a=0
 
 
 for istep=1:maxstep
@@ -29,23 +31,18 @@ for istep=1:maxstep
     t = (istep-1)*tau; 
     xplot(istep) = t;
     yplot(istep) = z;
-%     yplot2(istep) = y;
-    
+    m=mf+md
 
-    a = ve*dm/m;
-    m = m-dm;
-    v = v+a*tau;
-    z = z+v*tau
-    
-    
-    
     % If ball reaches ground (y<0), break out of the loop
-    if( m <= 0 )
-        xplot(istep+1) = t; % Record last values computed
-        yplot(istep+1) = z;
-        break; % Break out of the for loop
+    if( mf > 0 )
+        a = ve*dm/m - G*ME/((z+RE)^2);
+        mf = mf-dm;
+    else
+        a= -G*ME/(RE^2)
     end
 
+    v = v+a*tau;
+    z = z+v*tau;
 end
 
 plot1=plot(xplot,yplot,'-');
@@ -65,4 +62,5 @@ xlabel('t'); ylabel('z');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Changelog:
 % 1.0 Initialise program. Basic acceleration
+% 1.1 Added in dry mass and gravitational potential
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
