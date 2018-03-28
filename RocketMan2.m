7d452bf2d70aba364c0285c662708ac7a3ff339a
szonglong <E0004621@u.nus.edu>
2018-03-28 14:57:50 +0800

    2.00
    updated

--- RocketMan2.m
+++ RocketMan2.m
@@ -5,8 +5,6 @@
 % NOTE: clear variables before each run
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
 %static variables
 ve1=2500; ve2=3550;
 dm1=251; dm2=4.1; md1=20000; md2=4500;
@@ -18,12 +16,13 @@ A= 113;
 z0=0;
 v0=0;
 m0=407000; m1=15500;
+vt0=460; st0=0
 tau = 0.1; %interval
 maxstep = 6000; % no. of iterations
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %assigning variables to init values
-z=z0; v=v0; mf1=m0; a=0; mf2=m1;
+z=z0; v=v0; mf1=m0; a=0; mf2=m1; vt=vt0; st=st0;
 
 
 for istep=1:maxstep
@@ -31,6 +30,9 @@ for istep=1:maxstep
     t = (istep-1)*tau; 
     m=mf1+md1;
     rho=(1.225*exp(-(z)/8780));
+    vt=vt0+(RE/(RE+z))
+    
+    
     xplot(istep) = t;
     yplot(istep) = z;
 %     yplot(istep) = a/9.81;
@@ -39,7 +41,7 @@ for istep=1:maxstep
 
     % while mf > 0
     if( mf1 > 0 )
-        a_prime = ve1*(dm1/tau)/m - G*ME/((z+RE)^2) - (0.5*0.82*rho*A*v^2)/m;
+        a_prime = ve1*(dm1/tau)/m + vt^2/(RE+z) - G*ME/((z+RE)^2) - (0.5*0.82*rho*A*v^2)/m;
         if (a_prime>0)
             a = a_prime;
         else
@@ -51,6 +53,7 @@ for istep=1:maxstep
     end
     v = v+a*tau;
     z = z+v*tau;
+    st = st+vt*tau;
 end
 
 for istep=iter:maxstep
@@ -64,11 +67,11 @@ for istep=iter:maxstep
    
 
     if( mf2 > 0 )
-        a = ve2*(dm2/tau)/m - G*ME/((z+RE)^2) - (0.5*0.82*rho*A*v^2)/m;
+        a = ve2*(dm2/tau)/m + vt^2/(RE+z) - G*ME/((z+RE)^2) - (0.5*0.82*rho*A*v^2)/m;
         mf2 = mf2-dm2;
         f=istep
     else
-        a= -G*ME/((z+RE)^2) - (0.5*0.82*rho*A*v^2)/m;
+        a= -G*ME/((z+RE)^2) - (0.5*0.82*rho*A*v^2)/m + vt^2/(RE+z);
     end
     v = v+a*tau;
     z = z+v*tau;
@@ -99,5 +102,5 @@ xlabel('t'); ylabel('z');
 % 1.4 Verified with Falcon Heavy
 % 1.5 Added in stage 2
 
-% 2.0 Make 2D
+% 2.0 Make 2D: a_r, v_t
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
