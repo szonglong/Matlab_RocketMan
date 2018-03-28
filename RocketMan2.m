2960f1ae28b11bd4e5c58696c3bc7b84f80fe739
szonglong <E0004621@u.nus.edu>
2018-03-28 16:11:41 +0800

    2.3
    make neat

--- RocketMan2.m
+++ RocketMan2.m
@@ -26,22 +26,27 @@ z=z0; v=v0; mf1=m0; a=0; mf2=m1; vt=vt0; st=st0;
 
 
 for istep=1:maxstep
-    iter=istep;
     t = (istep-1)*tau; 
-    m=mf1+md1;
+    m=mf1+mf2+md2;
     rho=(1.225*exp(-(z)/8780));
     at=(-vt0)*(RE/(RE+z)^2)*v;
+    aFd=(0.5*0.82*rho*A*v^2)/m;
+    aFi1=ve1*(dm1/tau)/m;
+    aFi2=ve2*(dm2/tau)/m;
+    aFg=G*ME/((z+RE)^2);
+    aFc=vt^2/(RE+z);
+    theta=atan(vt/v);
     
     
     xplot(istep) = t;
-    yplot(istep) = st;
+    yplot(istep) = z;
 %     yplot(istep) = a/9.81;
 %     yplot(istep) = 0.5*rho*v^2;
    
 
     % while mf > 0
     if( mf1 > 0 )
-        a_prime = ve1*(dm1/tau)/m + vt^2/(RE+z) - G*ME/((z+RE)^2) - (0.5*0.82*rho*A*v^2)/m;
+        a_prime = aFi1 + aFc - aFg - aFd;
         if (a_prime>0)
             a = a_prime;
         else
@@ -49,31 +54,12 @@ for istep=1:maxstep
         end
         mf1 = mf1-dm1;
     else
-        break
-    end
-    v = v+a*tau;
-    z = z+v*tau;
-    vt = vt + at*tau;
-    st = st+vt*tau;
-end
-
-for istep=iter:maxstep
-    t = (istep-1)*tau; 
-    m=mf2+md2;
-    rho=(1.225*exp(-(z)/8780));
-    at=(-vt0)*(RE/(RE+z)^2)*v + atc;
-    
-    xplot(istep) = t;
-    yplot(istep) = st;
-%     yplot(istep) = a/9.81;
-%     yplot(istep) = 0.5*rho*v^2;
-   
-
-    if( mf2 > 0 )
-        a = ve2*(dm2/tau)/m + vt^2/(RE+z) - G*ME/((z+RE)^2) - (0.5*0.82*rho*A*v^2)/m;
-        mf2 = mf2-dm2;
-    else
-        a= -G*ME/((z+RE)^2) - (0.5*0.82*rho*A*v^2)/m + vt^2/(RE+z);
+        if( mf2 > 0 )
+            a = aFi2 + aFc - aFg - aFd;
+            mf2 = mf2-dm2;
+        else
+            a= -aFg - aFd + aFc;
+        end
     end
     v = v+a*tau;
     z = z+v*tau;
@@ -84,9 +70,6 @@ end
 
 plot1=plot(xplot,yplot,'-');
 xlabel('t'); ylabel('z');
-
-
-
 
 
 
@@ -107,4 +90,6 @@ xlabel('t'); ylabel('z');
 
 % 2.0 Make 2D: a_r, v_t
 % 2.1 a_t
+% 2.2 make single loop
+% 2.3 consolidated variables
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
