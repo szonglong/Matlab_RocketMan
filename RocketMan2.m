03f0f715f3cb052790abccec96be213514538a8c
szonglong <E0004621@u.nus.edu>
2018-04-09 19:00:33 +0800

    2.10
    array of vz given range of theta

--- RocketMan2.m
+++ RocketMan2.m
@@ -4,7 +4,16 @@
 % 
 % NOTE: clear variables before each run
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+array=zeros;
+index = 1;
+for i=1.6:0.1:2.1
+    array(index,:)=fun(i);
+    index = index +1;
+    i
+end
+array
 
+function f=fun(theta)
 %static variables
 ve1=2500; ve2=4600;%3550;%exhaust velocity
 dm1=251; dm2=4.1; md2=4500; %md1=20000; %dry mass and mass flow rate
@@ -29,7 +38,7 @@ at=0; %initial tangential velocity
 z=z0; vz=vz0; mf1=m0; az=0; mf2=m1; st=st0; vt=vt0; vrt=vrt0;
 
 %v2 "hand of God" theta
-theta=1.975;
+%theta=2.039239112185;
 
 for istep=1:maxstep
     t = (istep-1)*tau; 
@@ -45,7 +54,7 @@ for istep=1:maxstep
     
     xplot(istep) = t;
     yplot(istep) = z;
-    yplot2(istep)= (vt+vw0)-vreq;
+    yplot2(istep)= vz;%(vt+vw0)-vreq;
    
 
     % stage 1
@@ -64,9 +73,9 @@ for istep=1:maxstep
             mf2 = mf2-dm2;
             art = aFi2*sin(theta) - aFd*sin(theta);
         else
-            %break %when fuel runs out, cut the loop
-            az=  aFc -aFg - aFd;
-            art=- aFd*sin(theta);
+            break %when fuel runs out, cut the loop
+%             az=  aFc -aFg - aFd;
+%             art=- aFd*sin(theta);
         end
     end
     
@@ -80,10 +89,10 @@ for istep=1:maxstep
 end
 
 
-plot1=plot(xplot,yplot,'-',xplot,yplot2,'-');
-xlabel('t'); ylabel('z');
-
-
+% plot1=plot(xplot,yplot,'-',xplot,yplot2,'-');
+% xlabel('t'); ylabel('z');
+f=vz;
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Changelog:
@@ -107,6 +116,7 @@ xlabel('t'); ylabel('z');
 % redefined
 % 2.8 remove aw, explicit vw
 % 2.9 revised some expressions, should be a basic correct one
+% 2.10 returns array of vz for range of theta
 
 %to do:
 % alpha(t) st vr = 0, vt = vreq, z = zreq; -> single variable v3
